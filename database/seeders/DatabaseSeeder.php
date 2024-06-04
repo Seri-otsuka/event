3b15d66938c76155ca9118089e9cfb9502b1a0cb
Seri-otsuka <kd1326982@st.kobedenshi.ac.jp>
2024-06-04 11:19:15 +0900

    コピペ修正5

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use DateTime;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +16,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         DB::table('categories')->insert([
+            'name' => 'ANIME',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+            'deleted_at' => null,
+        ]);
+        
+        DB::table('categories')->insert([
+            'name' => 'K-POP',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+            'deleted_at' => null,
+        ]);
+        
+        DB::table('categories')->insert([
+            'name' => 'GAME',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+            'deleted_at' => null,
+        ]);
     }
 }
